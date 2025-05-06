cb0a3b0cc3b1a31ea35320e1c8cd466ebab97818
shaundeb <debnathshaun31@gmail.com>
2025-05-06 11:45:58 +0200

    Puntos linkados a la base datos + métodos de HandlePurchase

--- presentation/src/main/java/edu/ub/pis2425/projecte7owls/presentation/ShoppingFragment.java
+++ presentation/src/main/java/edu/ub/pis2425/projecte7owls/presentation/ShoppingFragment.java
@@ -7,13 +7,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -24,6 +27,7 @@ import java.util.List;
 import edu.ub.pis2425.projecte7owls.R;
 import edu.ub.pis2425.projecte7owls.domain.entities.Product;
 import edu.ub.pis2425.projecte7owls.presentation.adapters.ProductAdapter;
+import edu.ub.pis2425.projecte7owls.presentation.viewmodel.UserViewModel;
 
 public class ShoppingFragment extends Fragment implements ProductAdapter.OnProductClickListener {
 
@@ -33,20 +37,26 @@ public class ShoppingFragment extends Fragment implements ProductAdapter.OnProdu
     private ProductAdapter adapter;
     private List<Product> productList;
     private TextView textViewTotal;
+    private TextView pointsTextView;
     private Button buttonCheckout;
     private int totalPrice = 0;
 
+    private UserViewModel userViewModel;
+    private String uid;
     private FirebaseFirestore db;
+    private int currentPoints = 0;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_shopping, container, false);
 
+        uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
         recyclerView = view.findViewById(R.id.recyclerViewProducts);
         textViewTotal = view.findViewById(R.id.textViewTotal);
         buttonCheckout = view.findViewById(R.id.buttonCheckout);
-
+        userViewModel = new ViewModelProvider(requireActivity()).get(UserViewModel.class);
+        pointsTextView = view.findViewById(R.id.pointsTextView);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         productList = new ArrayList<>();
         adapter = new ProductAdapter(productList, this);
@@ -56,9 +66,20 @@ public class ShoppingFragment extends Fragment implements ProductAdapter.OnProdu
 
         loadProductsFromFirestore();
 
+        // Observa los puntos del usuario
+        userViewModel.observeUserScore(uid).observe(getViewLifecycleOwner(), score -> {
+            currentPoints = score != null ? score : 0;
+            pointsTextView.setText("Your points: " + currentPoints);
+        });
+
+
         buttonCheckout.setOnClickListener(v -> {
             if (getContext() != null) {
                 android.widget.Toast.makeText(getContext(), "Comprando productos: " + totalPrice + " points", android.widget.Toast.LENGTH_SHORT).show();
+                handlePurchase(totalPrice);
+                totalPrice = 0;
+                textViewTotal.setText("Total: 0 points");
+
             }
         });
 
@@ -89,4 +110,16 @@ public class ShoppingFragment extends Fragment implements ProductAdapter.OnProdu
         totalPrice += product.getPrice();
         textViewTotal.setText(String.format("Total: %d points", totalPrice));
     }
+
+    private void handlePurchase(int cost) {
+        if (currentPoints >= cost){
+            int updatePoints = currentPoints - cost;
+            userViewModel.updateUserScore(uid, updatePoints);
+            Toast.makeText(getContext(), "Items successfully purchased! - " + cost + " points", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getContext(), "Insufficient points", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 }
