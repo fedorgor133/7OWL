9a9563d9005594061a4a47cdce4ecce591a5cfcb
Sergi Catena Herrero <scatenhe7@alumnes.ub.edu>
2025-05-06 20:51:06 +0200

    Implementacion perfil + logos bottom_navigation actualizados

--- presentation/src/main/java/edu/ub/pis2425/projecte7owls/presentation/ProfileActivity.java
+++ presentation/src/main/java/edu/ub/pis2425/projecte7owls/presentation/ProfileActivity.java
@@ -2,26 +2,54 @@ package edu.ub.pis2425.projecte7owls.presentation;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.Timestamp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import edu.ub.pis2425.projecte7owls.R;
 import edu.ub.pis2425.projecte7owls.databinding.ActivityProfileBinding;
 
 public class ProfileActivity extends AppCompatActivity {
     private ActivityProfileBinding binding;
+    private FirebaseFirestore db;
+    private FirebaseAuth mAuth;
+    private int currentPoints;
+    private String uid;
+    private TextView pointsTextViewProfile;
+    private TextView emailTextView;
+    private TextView entryDateTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityProfileBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        pointsTextViewProfile = findViewById(R.id.pointsTextViewProfile);
+        emailTextView = findViewById(R.id.emailTextView);
+        entryDateTextView = findViewById(R.id.entryDateTextView);
+
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        uid = mAuth.getCurrentUser().getUid();
+
+        setupBottomNavigation();
+        loadUserPoints();
+        loadUserInfo();
     }
     private void setupBottomNavigation() {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setSelectedItemId(R.id.nav_contador);
+        bottomNavigationView.setSelectedItemId(R.id.nav_profile);
 
         bottomNavigationView.setOnItemSelectedListener(item -> {
             int id = item.getItemId();
@@ -42,5 +70,41 @@ public class ProfileActivity extends AppCompatActivity {
             }
             return false;
         });
+    }
+    private void loadUserPoints() {
+        if (mAuth.getCurrentUser() != null) {
+            db.collection("usuarios").document(uid).get().addOnSuccessListener(document -> {
+                if (document.exists() && document.contains("points")) {
+                    Long pointsValue = document.getLong("points");
+                    currentPoints = pointsValue != null ? pointsValue.intValue() : 0;
+                } else {
+                    currentPoints = 0;
+                }
+                pointsTextViewProfile.setText("Points: " + currentPoints);
+            }).addOnFailureListener(e -> {
+                Log.e("Firestore", "Error loading points", e);
+            });
+        }
+    }
+    private void loadUserInfo(){
+        if (mAuth.getCurrentUser() != null) {
+            db.collection("usuarios").document(uid).get().addOnSuccessListener(document -> {
+                if (document.exists()) {
+                    if (document.contains("email")) {
+                        emailTextView.setText(document.getString("email"));
+                    }
+                    if (document.contains("fechaRegistro")) {
+                        Timestamp timestamp = document.getTimestamp("fechaRegistro"); // Obtiene el Timestamp
+                        if (timestamp != null) {
+                            Date date = timestamp.toDate(); // Convierte a Date
+                            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()); // Formato de fecha
+                            entryDateTextView.setText(sdf.format(date)); // Muestra la fecha formateada
+                        }
+                    }
+                }
+            }).addOnFailureListener(e -> {
+                Log.e("Firestore", "Error loading userInfo", e);
+            });
+        }
     }
 }
